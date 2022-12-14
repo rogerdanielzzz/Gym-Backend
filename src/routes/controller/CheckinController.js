612abe34a33a78f462da30f61f1346dd1ad001c0
Roger Pérez <90121735+rogerdanielzzz@users.noreply.github.com>
2022-12-14 18:49:25 -0300

    Checkin idType added

--- src/routes/controller/CheckinController.js
+++ src/routes/controller/CheckinController.js
@@ -1,20 +1,20 @@
-const { Gym, Costumer, Checkin} = require("../../db.js");
-const { dateFormated} = require("../../utils/utils");
+const { Gym, Costumer, Checkin } = require("../../db.js");
+const { dateFormated } = require("../../utils/utils");
 
 let checkInRegister = async (req, res) => {
-    const {idType, idNumber, gymId} = req.body;
+    const { idType, idNumber, gymId } = req.body;
 
     let dateGetter = dateFormated()
     let dateArr = dateGetter.split("-")
-  
+
     let date = new Date();
     let hour = `${date.getHours()}:${date.getMinutes()}`
 
 
-    if((idNumber)||(idType)||(gymId)){
-        
+    if ((idNumber) || (idType) || (gymId)) {
+
         try {
-    
+
             let gym = await Gym.findOne({
                 where: {
                     id: gymId
@@ -29,47 +29,49 @@ let checkInRegister = async (req, res) => {
                 }
             })
 
-            let acepted= costumer.expire>dateGetter
-    
-    
-            let checkin = await Checkin.create({
-                acepted,
-                year: dateArr[0],
-                month: dateArr[1],
-                day: dateArr[2],
-                hour: hour
-            })
-    
-            await checkin.setGym(gym)
-            await checkin.setCostumer(costumer)
+            if (costumer && gym) {
+                let acepted = costumer.expire > dateGetter
+                let checkin = await Checkin.create({
+                    acepted,
+                    year: dateArr[0],
+                    month: dateArr[1],
+                    day: dateArr[2],
+                    hour: hour
+                })
 
-    
-            
-        
+                await checkin.setGym(gym)
+                await checkin.setCostumer(costumer)
+
                 res.status(201).json({ success: checkin })
-           
-          
-    
+
+            } else {
+                res.status(201).json({ error: "Cliente no Existe" })
+
+            }
+
+
+
+
         } catch (err) {
             console.log(err)
-            res.status(203).json({error:err});
-    
+            res.status(203).json({ error: err });
+
         }
 
 
-    }else{
+    } else {
 
         res.status(201).json({ msg: "Currency Must be provide gymId and Idnumber of costumer" })
 
 
 
     }
-    
+
 
 }
 
 
 module.exports = {
     checkInRegister,
-    
+
 }
