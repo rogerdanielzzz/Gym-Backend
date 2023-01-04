cef52f528322ea59cdbc55b875cb110420754767
Roger Pérez <90121735+rogerdanielzzz@users.noreply.github.com>
2023-01-04 13:04:38 -0300

    Soft delete Plan

--- src/routes/controller/PlanCrud.js
+++ src/routes/controller/PlanCrud.js
@@ -2,14 +2,14 @@ const { Gym, Plan } = require("../../db.js");
 const { toCapitalize } = require("../../utils/utils");
 
 let createPlan = async (req, res) => {
-    const {gymId, planName,durationUnit,durationQty,price} = req.body;
- 
-    if((durationUnit==="Day")||(durationUnit==="Week")||((durationUnit==="Month"))||((durationUnit==="Year"))){
+    const { gymId, planName, durationUnit, durationQty, price } = req.body;
+
+    if ((durationUnit === "Day") || (durationUnit === "Week") || ((durationUnit === "Month")) || ((durationUnit === "Year"))) {
 
         let nameCapitalized = toCapitalize(planName)
         let priceParsed = parseFloat(price)
-        
-   
+
+
 
         try {
             let gym = await Gym.findOne({
@@ -17,7 +17,7 @@ let createPlan = async (req, res) => {
                     id: gymId
                 }
             })
-    
+
             let plan = await Plan.create({
                 planName: nameCapitalized,
                 price: priceParsed,
@@ -25,66 +25,69 @@ let createPlan = async (req, res) => {
                 durationQty
 
             })
-    
+
             await plan.setGym(gym)
-    
-    
-        
-                res.status(201).json({ msg: "Plan price created" })
-           
-          
-    
+
+
+
+            res.status(201).json({ msg: "Plan price created" })
+
+
+
         } catch (err) {
             console.log(err)
-            res.status(203).json({error:err});
-    
+            res.status(203).json({ error: err });
+
         }
 
 
-    }else{
+    } else {
 
         res.status(201).json({ msg: "Please provide duration in Day , Week or Month" })
 
 
 
     }
-    
+
 
 }
 
-const getPlans= async(req, res)=>{
-    const {gymId}= req.body
-    if (gymId){
-    try {
-        
-        let plan = await Plan.findAll({
-            where: {
-                gymId,
-                active: true
-            }
-        })
+const getPlans = async (req, res) => {
+    const { gymId } = req.body
+    if (gymId) {
+        try {
 
-        res.status(200).json({plan,});
-    } catch (error) {
-        res.status(203).json({error:error});
-        
+            let plan = await Plan.findAll({
+                where: {
+                    gymId,
+                    active: true
+                }
+            })
+
+            res.status(200).json({ plan, });
+        } catch (error) {
+            res.status(203).json({ error: error });
+
+        }
+    } else {
+        res.status(203).json({ error: "gymId must be send by req.body" });
+
     }
-} else{
-    res.status(203).json({error: "gymId must be send by req.body"});
-   
-}
 }
 
 
 
-const softDeletePlan = async(req,res)=>{
-    const {planId}= req.body
-    try {
-        await Gym.update({ active:false }, { where: { id: planId} })
-        res.status(201).json({ success: "Gym Disabled" });
-    } catch (e) {
-        res.status(404).json({ error: e.message });
-    }
+const softDeletePlan = async (req, res) => {
+    const { planId } = req.body
+    if (planId) {
+        try {
+            await Gym.update({ active: false }, { where: { id: planId } })
+            res.status(201).json({ success: "Plan Disabled" });
+        } catch (e) {
+            res.status(404).json({ error: e.message });
+        }
+    }else res.status(404).json({ error: "planId must be Sent" });
+
 
 }
 module.exports = {
